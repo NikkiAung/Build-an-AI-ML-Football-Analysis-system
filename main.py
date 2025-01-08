e83c9492755fdf2f2a11d42855a2e09a672b57c6
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-01-08 00:01:06 -0800

    dynamically arounded the bottom circle wth team color

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
 import cv2
+from team_assigner import TeamAssigner
 
 def main():
     # Read Video
@@ -23,6 +24,20 @@ def main():
 
     #     cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
     #     break
+
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
     # Draw output 
     ## Draw object Tracks
